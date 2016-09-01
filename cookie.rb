f0f0d18cbe2a04181f2f6f646293be69e0e75284
Sysfinite <ryan@vidyagam.es>
2016-09-01 12:58:51 +0100

    Create cookie.rb

---
+++ cookie.rb
@@ -0,0 +1,20 @@
+require 'open3'
+# Acquire a fortune from a coo.kie.space/a.
+class Cookie < Plugin
+  def initialize
+    super
+    @needs_sanitization = true
+    @commands = %w(cookie)
+    @help_text = 'Get a randomly generated fortune'
+    @min_args = 0
+  end
+  def go(source, _command, args, bot)
+    result = nil
+    error = 'GELP'
+    Open3.popen3('curl', '--silent', 'http://coo.kie.space/a') do |_stdin, stdout, stderr|
+      result = stdout.read.chomp
+      error = stderr.read.chomp
+      bot.say(self, source, result)
+    end
+end
+end
